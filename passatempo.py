d047651b313fe60385ff0f522e53787be1d4a4b8
alpnista06 <mariod.b.filho@gmail.com>
2018-02-02 12:22:08 -0200

    adicionado mais um indice a lista de widgets

--- passatempo.py
+++ passatempo.py
@@ -12,7 +12,7 @@ class passatempo(GridLayout):
         self.cols = 4
         super(passatempo, self).__init__()
         self.labels = []
-        for indice in range(1,15):
+        for indice in range(1,16):
             self.labels.append(Label(text=str(indice)))
         
         for label in self.labels:
@@ -24,14 +24,13 @@ class passatempo(GridLayout):
     def on_touch_move(self, touch):
         touch.ud['line'].points += [touch.x, touch.y]
         
- ''' def on_touch_down(self, touch):
+    def on_touch_down(self, touch):
         color = (random(), 1, 1)
         with self.canvas:
             Color(*color, mode='hsv')
             d = 30.
             Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
             touch.ud['line'] = Line(points=(touch.x, touch.y))
-'''
        
 class passaApp(App):    
     def build(self):    
