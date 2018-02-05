788486a183564f84bcb3c4fd175efa967ab94cca
alpnista06 <mariod.b.filho@gmail.com>
2018-02-05 18:42:13 -0200

    tentando inserir botoes e move-los

--- passatempo.py
+++ passatempo.py
@@ -1,8 +1,11 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.uix.scatterlayout import ScatterLayout
+from kivy.graphics import *
 
-
+        
+        
 class passatempo(GridLayout):
     def __init__(self):
         self.cols = 4
@@ -13,9 +16,28 @@ class passatempo(GridLayout):
         
     def callback(self, instance, value):
         for indice in range(1,16):
-            self.buttons[indice].bind(on_press=passatempo)
+            self.buttons[indice].bind(on_press=movimento)
+
+'''
+class toque(Button):
+    def moveNode(self):
+        with touch:
+            self.pos=[touch.x-25, touch.y-25]
 
 
+       #def onTouchMove(self, touch):
+       #   if self.collide_point(touch.x, touch.y):
+       #       self.pos=[touch.x-25, touch.y-25]
+        pass   
+'''
+
+
+class movimento(ScatterLayout):
+    def mov(self):
+           self.pos=[touch.x-25, touch.y-25]
+        
+    
+    
 class passaApp(App):    
     def build(self):    
         return passatempo()  
